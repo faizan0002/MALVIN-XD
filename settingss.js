4120846a594cdfd17badc23b50d807d22a5d11a7
faizan0002 <faizanf80824@gmail.com>
2025-08-16 09:36:12 -0700

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~UmFVABDL#H0ByDG2pLiViPzhvqHIndWWN1xdDT1LZJ4xnluC-Ch0",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923296269525", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Pakistan/Islamabad" //put your country timeZone....leave blank if u don't know.
 };
